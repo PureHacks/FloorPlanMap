faeb8367d85d6a4d8666a8fef825574251f0d35b
Tim Ludikar <tim.ludikar@ikandimedia.ca>
2014-12-13 11:49:02 -0500

    Adding relationship between Locaiton and Employee

--- models/location.js
+++ models/location.js
@@ -3,7 +3,7 @@ var mongoose = require('../env/mongoose.js');
 var locationSchema = mongoose.Schema({
 	type: String,
 	location: String
-});
+}); 
 
 var LocationItem = mongoose.model('Location', locationSchema);
 
